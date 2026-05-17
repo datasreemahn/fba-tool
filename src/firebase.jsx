d06327fe85f5103bf212b6647159269765d8518b
Sree Hari Subhash <sreemahn@outlook.com>
2026-05-17 13:47:16 -0500

    Fix unused analytics variable in firebase.jsx

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAnalytics } from "firebase/analytics";
+
 
 const firebaseConfig = {
   apiKey: "AIzaSyAjTy2TmWfwQUht9DMc53eFofPm3RSOqkA",
@@ -8,10 +8,8 @@ const firebaseConfig = {
   projectId: "fba-command-center-9986b",
   storageBucket: "fba-command-center-9986b.firebasestorage.app",
   messagingSenderId: "680825501808",
-  appId: "1:680825501808:web:f5b616f161c7d276fbdcb5",
-  measurementId: "G-3P4HRDH3HX"
+  appId: "1:680825501808:web:f5b616f161c7d276fbdcb5"
 };
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-const analytics = getAnalytics(app);
